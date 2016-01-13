dc3955e2e3a86cfb09a29027bbe4c4b11228523c
Artur Kozak <artur.kozak@students.mimuw.edu.pl>
2016-01-13 17:59:52 +0100

    refactor locations to be a normal class

--- src/LatteCodes.py
+++ src/LatteCodes.py
@@ -93,19 +93,19 @@ class Codes(object):
         """ Generator of assembly instructions from a single intermediate code. """
         for case in switch(code['type']):
             if case(cls.PUSH):
-                yield cls._str_asm('pushl', [Loc._str(code['src'])], code)
+                yield cls._str_asm('pushl', [str(code['src'])], code)
                 return
             if case(cls.POP):
-                yield cls._str_asm('popl', [Loc._str(Loc.reg(code['reg']))], code)
+                yield cls._str_asm('popl', [str(Loc.reg(code['reg']))], code)
                 return
             if case(cls.MOV):
-                yield cls._str_asm('movl', [Loc._str(code['src']), Loc._str(code['dest'])], code)
+                yield cls._str_asm('movl', [str(code['src']), str(code['dest'])], code)
                 return
             if case(cls.JUMP):
                 yield cls._str_asm('jmp', [code['dest']], code)
                 return
             if case(cls.IF_JUMP):
-                yield cls._str_asm('cmpl', [Loc._str(code['lhs']), Loc._str(code['rhs'])], code)
+                yield cls._str_asm('cmpl', [str(code['lhs']), str(code['rhs'])], code)
                 yield cls._str_asm(code['op'], [code['dest']], code)
                 return
             if case(cls.LABEL):
@@ -118,7 +118,7 @@ class Codes(object):
                 yield cls._str_asm('pushl', ['%ebp'], code)
                 yield cls._str_asm('movl', ['%esp', '%ebp'], code)
                 var_space = Loc.const((code['var_count'] + 1) * Codes.var_size)
-                yield cls._str_asm('subl', [Loc._str(var_space), '%esp'], code)
+                yield cls._str_asm('subl', [str(var_space), '%esp'], code)
                 return
             if case(cls.LEAVE):
                 yield cls._str_asm('leave', [], code)
@@ -126,19 +126,19 @@ class Codes(object):
                 return
             if case(cls.ADD, cls.SUB, cls.MUL):
                 op = {cls.ADD: 'addl', cls.SUB: 'subl', cls.MUL: 'imull'}[code['type']]
-                yield cls._str_asm(op, [Loc._str(code['lhs']), Loc._str(code['rhs'])], code)
+                yield cls._str_asm(op, [str(code['lhs']), str(code['rhs'])], code)
                 return
             if case(cls.DIV):
                 yield cls._str_asm('cdq', [], code)
-                yield cls._str_asm('idivl', [Loc._str(code['lhs'])], code)
+                yield cls._str_asm('idivl', [str(code['lhs'])], code)
                 return
             if case(cls.NEG):
-                yield cls._str_asm('negl', [Loc._str(code['dest'])], code)
+                yield cls._str_asm('negl', [str(code['dest'])], code)
                 return
             if case(cls.BOOL_OP):
-                yield cls._str_asm('cmpl', [Loc._str(code['lhs']), Loc._str(code['rhs'])], code)
+                yield cls._str_asm('cmpl', [str(code['lhs']), str(code['rhs'])], code)
                 yield cls._str_asm(code['op'], [Loc.regcmp], code)
-                yield cls._str_asm('movzbl', [Loc.regcmp, Loc._str(code['dest'])], code)
+                yield cls._str_asm('movzbl', [Loc.regcmp, str(code['dest'])], code)
                 return
             if case(cls.ASM):
                 yield cls._str_asm(code['parts'][0], code['parts'][1:], code)
@@ -149,12 +149,21 @@ class Codes(object):
             if case(cls.DELETED):  # TODO remove this, DELETED codes should be really deleted
                 d = code.copy()
                 del d['type']
-                yield '\t# [deleted] ' + str(sorted(d.items()))
+                yield '\t# [deleted] ' + cls._str_code(d)
                 return
             if case(cls.CHILD):
                 raise InternalError('code type %s not allowed here', cls._code_name(code['type']))
             if case():
                 raise InternalError('invalid code: ' + str(code))
+
+    @classmethod
+    def _str_code(cls, code):
+        res = []
+        for key, value in sorted(code.iteritems()):
+            val_str = str(value) if not isinstance(value, str) else value
+            res.append(key + ': ' + val_str)
+        return ", ".join(res)
+
 
     @classmethod
     def _str_asm(cls, instr, args, code):
@@ -181,24 +190,43 @@ class Loc(object):
     ebp = '%ebp'
     regcmp = '%al'
 
-    @staticmethod
-    def const(n):
-        return {'const': n}
+    # type tokens
+    CONST = 'const'
+    REG = 'reg'
+    MEM = 'mem'
+    STRINGLIT = 'stringlit'
 
-    @staticmethod
-    def reg(which):
-        return {'reg': which}
+    # special token for value matching
+    ANY = '__any__'
+
+    def __init__(self, type, value):
+        self.type = type
+        self.value = value
+
+    # factory methods
+    @classmethod
+    def const(cls, n):
+        return cls(cls.CONST, n)
+
+    @classmethod
+    def reg(cls, which):
+        return cls(cls.REG, which)
 
     @classmethod
     def mem(cls, addr, offset=None):
-        if addr[-1] == ')':
-            return {'mem': addr}
-        return {'mem': cls.mkaddr(addr, offset)}
+        if addr == cls.ANY:
+            return cls(cls.MEM, cls.ANY)
+        if addr[-1] == ')':  # already computed address
+            return cls(cls.MEM, addr)
+        return cls(cls.MEM, cls.mkaddr(addr, offset))
 
-    @staticmethod
-    def stringlit(obj):
-        return {'stringlit': Codes.string_literal_label(obj)}
+    @classmethod
+    def stringlit(cls, obj):
+        if obj == cls.ANY:
+            return cls(cls.STRINGLIT, cls.ANY)
+        return cls(cls.STRINGLIT, Codes.string_literal_label(obj))
 
+    # helper methods
     @classmethod
     def arg_addr(cls, n):
         """ Address of n-th function argument. """
@@ -213,24 +241,32 @@ class Loc(object):
     def mkaddr(pos, offset=None):
         return '%s(%s)' % (str(offset or ''), pos)
 
-    @classmethod
-    def _str(cls, loc):
-        """ Convert objects returned by reg(), mem(), etc to string. """
-        if 'const' in loc:
-            return '$' + str(loc['const'])
-        if 'reg' in loc:
-            r = loc['reg']
-            if r in ['a', 'b', 'c', 'd']:
-                return '%e' + r + 'x'
-            if r == 'cmp':
-                return '%al'
-            if r == 'top':
-                return '%esp'
-            if r in ['edi', 'esi', 'ebp']:
-                return '%' + r
-            raise InternalError('invalid register name: `%s`' % r)
-        if 'mem' in loc:
-            return loc['mem']
-        if 'stringlit' in loc:
-            return '$' + loc['stringlit']
-        raise InternalError('invalid loc: ' + str(loc))
+    def __str__(self):
+        for case in switch(self.type):
+            if case(self.CONST):
+                return '$' + str(self.value)
+            if case(self.REG):
+                r = self.value
+                if r in ['a', 'b', 'c', 'd']:
+                    return '%e' + r + 'x'
+                if r == 'cmp':
+                    return '%al'
+                if r == 'top':
+                    return '%esp'
+                if r in ['edi', 'esi', 'ebp']:
+                    return '%' + r
+                raise InternalError('invalid register name: `%s`' % r)
+            if case(self.MEM):
+                return self.value
+            if case(self.STRINGLIT):
+                return '$' + self.value
+            raise InternalError('invalid loc type: ' + self.type)
+
+    def __eq__(self, other):
+        """ Location matching, including possible `ANY` values. """
+        if isinstance(other, Loc):
+            if self.type != other.type:
+                return false
+            return self.value == self.ANY or other.value == self.ANY or self.value == other.value
+        return NotImplemented
+
